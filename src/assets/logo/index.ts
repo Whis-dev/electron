f694087ebd169a3292c91275655715ac48c7190c
whis <whisdevj@gmail.com>
2023-01-24 21:29:26 +0900

    [assets] - add assets

---
+++ src/assets/logo/index.ts
@@ -0,0 +1,5 @@
+import { ReactComponent as LogoLOL24X24 } from './logo-lol-24x24.svg';
+import { ReactComponent as LogoValorant24x24 } from './logo-valorant-24x24.svg';
+import { ReactComponent as LogoOPGG48x48 } from './logo-op-gg-48x48.svg';
+
+export { LogoLOL24X24, LogoValorant24x24, LogoOPGG48x48 };
